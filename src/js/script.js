acb3232ba39f12dce28b4d3e7748fc7b41c89d2c
lohvina <anhelina.lohvina@ut.ee>
2022-10-30 19:55:54 +0200

    initial commit

--- src/js/script.js
+++ src/js/script.js
@@ -58,3 +58,21 @@ fetch('./res/json/post_json.json')
     }
 
 })
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.logo-image')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
